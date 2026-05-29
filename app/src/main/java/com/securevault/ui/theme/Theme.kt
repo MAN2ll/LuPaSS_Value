8bdc5361d7460671881d8079608354e4b06b08d9
MAN2ll <a.n.moiseev89@gmail.com>
2026-05-29 18:58:04 +0300

    Update Theme.kt

--- app/src/main/java/com/securevault/ui/theme/Theme.kt
+++ app/src/main/java/com/securevault/ui/theme/Theme.kt
@@ -1,6 +1,6 @@
 package com.securevault.ui.theme
 
-import android.isAtLeastS
+// import android.isAtLeastS
 import androidx.compose.animation.Crossfade
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.isSystemInDarkTheme
